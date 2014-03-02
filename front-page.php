1d561b8e08e4d3d0f47a3c71da764d9b0e483927
Rodney Dennis <rdnydnns@gmail.com>
2014-03-01 21:49:35 -0600

    add untracked files

---
+++ front-page.php
@@ -0,0 +1,14 @@
+<?php get_header() ?>
+  <div class="hero container-slim pad-sides">
+    <img class="logo" src="wp-content/themes/abx-theme/images/logo.png"/>
+  </div>
+
+  <div id="maximage" class="visible-medium">
+    <img src="wp-content/themes/abx-theme/images/h01.jpg"/>
+    <img src="wp-content/themes/abx-theme/images/h02.jpg"/>
+    <img src="wp-content/themes/abx-theme/images/h03.jpg"/>
+    <img src="wp-content/themes/abx-theme/images/h07.jpg"/>
+    <img src="wp-content/themes/abx-theme/images/h08.jpg"/>
+    <img src="wp-content/themes/abx-theme/images/h09.jpg"/>
+  </div>
+<?php get_footer() ?>
